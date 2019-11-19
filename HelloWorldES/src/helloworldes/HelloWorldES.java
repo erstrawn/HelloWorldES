1d96725960ccb335a97d77643572d8caef95c7a3
erstrawn <erstrawn@sjs.org>
2019-11-19 14:24:59 -0600

    tencount

--- HelloWorldES/src/helloworldes/HelloWorldES.java
+++ HelloWorldES/src/helloworldes/HelloWorldES.java
@@ -15,7 +15,10 @@ public class HelloWorldES {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("yeet");
+        System.out.println("Hello World");
+        for(int i = 1; i < 11; i ++){
+            System.out.println(i);
+        }
     }
     
 }
